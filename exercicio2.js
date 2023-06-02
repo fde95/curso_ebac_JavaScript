bdeb54e1b0628df5996e98bd076c8893945291f2
Felipe Espinoza <domingues.espinoza@outlook.com>
2023-06-02 11:06:30 -0300

    Respondendo Exercicio 2 Mod 2

---
+++ exercicio2.js
@@ -0,0 +1,34 @@
+let branco = "preto";
+let preto = "cinza";
+let cinza = "branco";
+let carro = "preto";
+let valor = 30000;
+let prestacao = 750;
+
+// a) branco == “branco”
+console.log(branco == "branco"); 
+//Resp.: False - Porque branco tem valor 'preto' e não 'branco'.
+
+// b) branco == cinza
+console.log(branco == cinza);
+//Resp.: False - Porque branco tem valor 'preto' e cinza tem valor 'branco'.
+
+// c) carro === branco
+console.log(carro === branco);
+//Resp.: True - Pois ambas possuem o valor 'preto'.
+
+// d) var cavalo = carro == “preto” ? “cinza” : “marron”;
+let cavalo = carro == "preto" ? "cinza" : "marrom"
+console.log(cavalo);
+//Resp.: "cinza" - Pois carro tem o valor 'preto', então retorna 'cinza'.
+
+// e) Quantas prestações são necessárias para pagar o valor do carro com uma entrada de 3.000? Demonstre a operação.
+let qntParcelas;
+qntParcelas = valor / prestacao //retornará 40.
+console.log("Você precisará pagar " + qntParcelas + " parcelas até quitar o valor do carro.");
+//Resp.: 40 parcelas.
+
+// f) Somando as variáveis de cores é formada uma string de quantos caracteres?
+let somaCores = branco + preto + cinza;
+console.log("A soma das variveis de cores, gera uma String de " + somaCores.length + " caracteres")
+//Resp.: 16 Caracteres.
